b4eff17a93338c4342eaa39c163c7f2eadd37111
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2024-10-13 19:22:26 +0530

    Time: 5 ms (44.25%) | Memory: 42.2 MB (35.92%) - LeetSync

---
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public List<String> stringMatching(String[] words) {
+        List<String> result = new ArrayList<>();
+        
+        for (String ele1 : words) {
+            for (String ele2 : words) {
+                if (!ele1.equals(ele2) && ele2.contains(ele1)) {
+                    result.add(ele1);
+                    break;
+                }
+            }
+        }
+        
+        return result;
+    }
+}
