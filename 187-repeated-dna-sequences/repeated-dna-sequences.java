c3d668828f0829bd184e64a969fc7f46e3c83c74
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2024-10-16 00:53:48 +0530

    Time: 2 ms (100.00%) | Memory: 44.6 MB (98.78%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.java
+++ 187-repeated-dna-sequences/repeated-dna-sequences.java
@@ -1,11 +1,28 @@
+// class Solution {
+//     public List<String> findRepeatedDnaSequences(String s) {
+//         Set seen = new HashSet(), repeated = new HashSet();
+//         for (int i = 0; i + 9 < s.length(); i++) {
+//             String ten = s.substring(i, i + 10);
+//             if (!seen.add(ten))
+//                 repeated.add(ten);
+//         }
+//         return new ArrayList(repeated);
+//     }
+// }
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        Set seen = new HashSet(), repeated = new HashSet();
-        for (int i = 0; i + 9 < s.length(); i++) {
-            String ten = s.substring(i, i + 10);
-            if (!seen.add(ten))
-                repeated.add(ten);
+        List<String> list = new ArrayList<String>();
+        Set<String> seq = new HashSet<>();
+        int len = s.length();
+        if (len < 10 || len > 10000)
+            return list;
+        for (int i = 0; i <= len - 10; i++) {
+            String pattern = s.substring(i, i + 10);
+            if (!seq.add(pattern)) {
+                if (!list.contains(pattern))
+                    list.add(pattern);
+            }
         }
-        return new ArrayList(repeated);
+        return list;
     }
 }
