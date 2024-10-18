13343412ceff1f68fa31f03c354294286fd7d5f9
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2024-10-18 21:19:11 +0530

    Time: 0 ms (100.00%) | Memory: 49 MB (46.00%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -1,13 +1,25 @@
+// class Solution {
+//     public void reverseString(char[] s) {
+//         reverse(s, 0);
+//     }
+//     void reverse(char[] s, int i){
+//         int n=s.length;
+//         if(i==n/2) return;
+//         char temp=s[i];
+//         s[i]=s[n-i-1];
+//         s[n-i-1]=temp;
+//         reverse(s, i+1);
+//     }
+// }
+
+
 class Solution {
     public void reverseString(char[] s) {
-        reverse(s, 0);
-    }
-    void reverse(char[] s, int i){
-        int n=s.length;
-        if(i==n/2) return;
-        char temp=s[i];
-        s[i]=s[n-i-1];
-        s[n-i-1]=temp;
-        reverse(s, i+1);
+        int left = 0,right = s.length - 1;
+        while (left < right) {
+            char temp = s[right];
+            s[right--] = s[left];
+            s[left++] = temp;
+        }
     }
 }
