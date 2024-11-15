3a22f94b488a6fa68ac82c5959611916d2c10451
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2024-11-15 09:15:07 +0530

    Time: 1 ms (98.82%) | Memory: 41.9 MB (98.29%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.java
+++ 257-binary-tree-paths/binary-tree-paths.java
@@ -1,28 +1,33 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
-        List<String> paths = new ArrayList<>();
-        if(root != null) searchBT(root, "", paths);
-        return paths;
+        
+        List<String> list = new ArrayList<>();
+        StringBuilder path = new StringBuilder();        
+        
+        binaryTreePathsHelper(root,path,list);
+        return list;
+        
     }
-
-    private void searchBT(TreeNode root, String path, List<String> paths){
-        if(root.left == null && root.right == null) paths.add(path + root.val);
-        if(root.left !=null) searchBT(root.left, path+root.val + "->", paths);
-        if(root.right !=null) searchBT(root.right, path+root.val + "->", paths);
-    }
+    
+    public void binaryTreePathsHelper(TreeNode root,StringBuilder path,List<String> list){
+        
+        if(root == null) return ;
+        
+        int len = path.length();// inital length nikal li path ki taki path.setLength se backtracking kar sake
+        path.append(root.val);
+        
+        if(root.left==null && root.right==null){            
+            list.add(path.toString());
+            path.setLength(len);// backtracking before return                                    
+            return ;
+        }
+        
+        path.append("->");
+        
+        binaryTreePathsHelper(root.left,path,list);
+        binaryTreePathsHelper(root.right,path,list);
+        
+        path.setLength(len);// backtracking before return  
+    }        
+    
 }
