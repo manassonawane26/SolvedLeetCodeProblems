d901f2440c4e213c6515d8b717823884b138a25d
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2024-11-17 00:32:16 +0530

    Time: 1 ms (100.00%) | Memory: 45.9 MB (6.12%) - LeetSync

--- 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.java
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.java
@@ -1,33 +1,68 @@
+// class Solution {
+//     public int[] resultsArray(int[] nums, int k) {
+//         if (k == 1) {
+//             return nums.clone();
+//         }
+        
+//         int n = nums.length;
+//         List<Integer> result = new ArrayList<>();
+//         Deque<Integer> window = new ArrayDeque<>();
+        
+//         for (int i = 0; i < n; i++) {
+//             // Remove elements outside window
+//             while (!window.isEmpty() && i - window.peekFirst() >= k) {
+//                 window.pollFirst();
+//             }
+            
+//             // Check consecutive sequence
+//             if (window.isEmpty() || nums[i] - nums[i-1] == 1) {
+//                 window.offerLast(i);
+//             } else {
+//                 window.clear();
+//                 window.offerLast(i);
+//             }
+            
+//             // Add result when window size is k
+//             if (i >= k-1) {
+//                 result.add(window.size() == k ? nums[i] : -1);
+//             }
+//         }
+        
+//         return result.stream().mapToInt(Integer::intValue).toArray();
+//     }
+// }
+
+
 class Solution {
-    public int[] resultsArray(int[] nums, int k) {
-        if (k == 1) {
-            return nums.clone();
-        }
-        
+    public int[] resultsArray(int[] nums, int k) 
+    {
         int n = nums.length;
-        List<Integer> result = new ArrayList<>();
-        Deque<Integer> window = new ArrayDeque<>();
-        
-        for (int i = 0; i < n; i++) {
-            // Remove elements outside window
-            while (!window.isEmpty() && i - window.peekFirst() >= k) {
-                window.pollFirst();
-            }
-            
-            // Check consecutive sequence
-            if (window.isEmpty() || nums[i] - nums[i-1] == 1) {
-                window.offerLast(i);
-            } else {
-                window.clear();
-                window.offerLast(i);
-            }
-            
-            // Add result when window size is k
-            if (i >= k-1) {
-                result.add(window.size() == k ? nums[i] : -1);
+        int subArrCount = n-k+1;
+        int[] subArrPower = new int[n-k+1];
+        int inconsecutiveElementCount = 0;
+        for(int i=1; i<k; i++)
+        {
+            if(nums[i] != nums[i-1]+1)
+            {
+                inconsecutiveElementCount++;
             }
         }
+        int start =0, end = k-1;
+        while(end<n)
+        {
+            subArrPower[start] = inconsecutiveElementCount == 0 ? nums[end] : -1;
+            if(start<n-1 && nums[start] != nums[start+1]-1)
+            {
+                inconsecutiveElementCount--;
+            }
+            start++;
+            if(end<n-1 && nums[end+1] != nums[end]+1)
+            {
+                inconsecutiveElementCount++;
+            }
+            end++;
+        }
+        return subArrPower;
         
-        return result.stream().mapToInt(Integer::intValue).toArray();
     }
 }
