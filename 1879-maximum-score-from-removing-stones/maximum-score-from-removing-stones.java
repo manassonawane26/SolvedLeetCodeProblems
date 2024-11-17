9f378429556573f53f90da093ac40c7553b5ce5c
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2024-11-17 23:41:26 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (70.04%) - LeetSync

--- 1879-maximum-score-from-removing-stones/maximum-score-from-removing-stones.java
+++ 1879-maximum-score-from-removing-stones/maximum-score-from-removing-stones.java
@@ -1,18 +1,34 @@
+// class Solution {
+//     public int maximumScore(int a, int b, int c) {
+//         PriorityQueue<Integer> pq = new PriorityQueue<>(3, Comparator.reverseOrder());
+//         pq.offer(a);
+//         pq.offer(b);
+//         pq.offer(c);
+//         int count= 0;
+//         while(pq.size()>1){
+//             int x = pq.poll();
+//             int y = pq.poll();
+//             count++;
+//             x--; y--;
+//             if(x>0) pq.offer(x);
+//             if(y>0) pq.offer(y);
+//         }
+//         return count;
+//     }
+// }
+
+
+import java.util.Arrays;
+
 class Solution {
     public int maximumScore(int a, int b, int c) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(3, Comparator.reverseOrder());
-        pq.offer(a);
-        pq.offer(b);
-        pq.offer(c);
-        int count= 0;
-        while(pq.size()>1){
-            int x = pq.poll();
-            int y = pq.poll();
-            count++;
-            x--; y--;
-            if(x>0) pq.offer(x);
-            if(y>0) pq.offer(y);
+        int[] piles = new int[] {a, b, c};
+        Arrays.sort(piles);
+
+        if (piles[0] + piles[1] <= piles[2]) {
+            return piles[0] + piles[1];
         }
-        return count;
+        
+        return (a + b + c) / 2;
     }
 }
