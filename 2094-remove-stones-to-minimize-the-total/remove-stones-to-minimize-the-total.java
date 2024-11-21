09701d5eee713427d9542be663f3dcf76c160884
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2024-11-21 23:18:07 +0530

    Time: 419 ms (62.76%) | Memory: 57.5 MB (87.78%) - LeetSync

--- 2094-remove-stones-to-minimize-the-total/remove-stones-to-minimize-the-total.java
+++ 2094-remove-stones-to-minimize-the-total/remove-stones-to-minimize-the-total.java
@@ -1,17 +1,21 @@
 class Solution {
     public int minStoneSum(int[] piles, int k) {
         PriorityQueue<Integer> pq = new PriorityQueue<>(piles.length, Comparator.reverseOrder());
-        for(int i: piles)
+        int sum=0;
+        for(int i: piles){
             pq.offer(i);
+            sum+=i;
+        }
         
         for(int j =0;j<k; j++){
             int temp = pq.poll();
-            temp = (int) temp - (int)Math.floor(temp/2);
+            sum = sum - temp/2;
+            temp = temp - temp/2;
             pq.offer(temp);
         }
-        int sum=0;
-        while(!pq.isEmpty())
-            sum+=pq.poll();
+        
+        // while(!pq.isEmpty())
+        //     sum+=pq.poll();
         return sum;
     }
 }
