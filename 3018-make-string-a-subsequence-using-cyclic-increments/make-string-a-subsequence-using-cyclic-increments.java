4e6fac4914ca45da20c31b1d65b6aa55d4fbb6f2
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2024-12-04 23:31:26 +0530

    Time: 6 ms (71.31%) | Memory: 45.6 MB (53.29%) - LeetSync

--- 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.java
+++ 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.java
@@ -1,12 +1,32 @@
-class Solution {
-    public boolean canMakeSubsequence(String source, String target) {
-        int targetIdx = 0, targetLen = target.length();
+// class Solution {
+//     public boolean canMakeSubsequence(String source, String target) {
+//         int targetIdx = 0, targetLen = target.length();
         
-        for (char currChar : source.toCharArray()) {
-            if (targetIdx < targetLen && (target.charAt(targetIdx) - currChar + 26) % 26 <= 1) {
-                targetIdx++;
-            }
+//         for (char currChar : source.toCharArray()) {
+//             if (targetIdx < targetLen && (target.charAt(targetIdx) - currChar + 26) % 26 <= 1) {
+//                 targetIdx++;
+//             }
+//         }
+//         return targetIdx == targetLen;
+//     }
+// }
+
+class Solution {
+    char lessChar(char ch) {
+            return ch == 'a' ? 'z' : (char)(ch - 1);
         }
-        return targetIdx == targetLen;
+    public boolean canMakeSubsequence(String str1, String str2) {
+        int i = 0, j = 0;
+
+        
+        
+        while (i < str1.length() && j < str2.length()) {
+            if (str1.charAt(i) == lessChar(str2.charAt(j)) || str1.charAt(i) == str2.charAt(j)) {
+                j++;
+            }
+            i++;
+        }
+        
+        return j == str2.length();
     }
 }
