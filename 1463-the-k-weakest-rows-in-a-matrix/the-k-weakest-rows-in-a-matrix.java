a92770c90982b15ed3c9d8b80d9e6dcbe08ea18b
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2024-12-24 10:05:41 +0530

    Time: 1 ms (99.64%) | Memory: 44.9 MB (47.82%) - LeetSync

--- 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
@@ -4,12 +4,24 @@ class Solution {
 
         for(int i=0;i<mat.length;i++){
             int sum=0;
-            for(int j=0;j<mat[0].length;j++){
-                if(mat[i][j]==1){
-                    sum++;
+            int left = 0, right = mat[i].length - 1;
+
+            // Perform binary search to find the count of soldiers
+            while (left <= right) {
+                int mid = (left + right) / 2;
+                if (mat[i][mid] == 1) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
                 }
-                else break;
             }
+            sum = left;
+            // for(int j=0;j<mat[0].length;j++){
+            //     if(mat[i][j]==1){
+            //         sum++;
+            //     }
+            //     else break;
+            // }
             pq.add(sum*100+i);
         }
         int ans[]=new int[k];
