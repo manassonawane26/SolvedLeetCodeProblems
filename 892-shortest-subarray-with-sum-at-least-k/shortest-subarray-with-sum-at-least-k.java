a487aaaed846f28af6ae6d2c5589c045fefa04bd
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2025-01-07 21:54:04 +0530

    Time: 88 ms (16.37%) | Memory: 57 MB (97.10%) - LeetSync

--- 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.java
+++ 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.java
@@ -1,3 +1,33 @@
+class Pair{
+    long val;
+    long idx;
+    Pair(long a, long b){
+        val = a;
+        idx = b;
+    }
+}
+class Solution {
+    public int shortestSubarray(int[] nums, int k) {
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)-> (int) (a.val - b.val));
+        // PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->((int)(a.val-b.val)));
+
+        long n = nums.length;
+        long min = Long.MAX_VALUE;
+        long sum = 0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            if(sum>=k)
+                min = Math.min(min, i+1);
+
+            while(!pq.isEmpty() && sum - pq.peek().val >= k)
+                min = Math.min(min, i - pq.poll().idx);
+            
+            pq.offer(new Pair(sum, i));
+        }
+        return min != Long.MAX_VALUE ? (int) min : -1;
+
+    }
+}
 // class Solution {
 //     public int shortestSubarray(int[] nums, int k) {
 //         int count =0;
@@ -10,33 +40,33 @@
 //         return -1;
 //     }
 // }
-class Solution {
-    public int shortestSubarray(int[] nums, int k) {
+// class Solution {
+//     public int shortestSubarray(int[] nums, int k) {
         
-        int nLen = nums.length;
-        int shortest = nLen + 1;
+//         int nLen = nums.length;
+//         int shortest = nLen + 1;
         
-		// Building a prefix/cummulative sum of all elements
-        long[] prefixSum = new long[nLen + 1];
-        for(int i=0; i < nLen; i++)
-            prefixSum[i+1] = prefixSum[i] + nums[i];
+// 		// Building a prefix/cummulative sum of all elements
+//         long[] prefixSum = new long[nLen + 1];
+//         for(int i=0; i < nLen; i++)
+//             prefixSum[i+1] = prefixSum[i] + nums[i];
         
-		// Initialize the Deque for storing the starting indices
-        Deque<Integer> startIdxs = new ArrayDeque<>();
+// 		// Initialize the Deque for storing the starting indices
+//         Deque<Integer> startIdxs = new ArrayDeque<>();
         
-        for(int i=0; i < nLen + 1; i++){
-            // Finding the smallest window whose sum >= k
-            while(!startIdxs.isEmpty() && prefixSum[i] - prefixSum[startIdxs.peek()] >= k)
-                shortest = Math.min(shortest, i - startIdxs.poll());
+//         for(int i=0; i < nLen + 1; i++){
+//             // Finding the smallest window whose sum >= k
+//             while(!startIdxs.isEmpty() && prefixSum[i] - prefixSum[startIdxs.peek()] >= k)
+//                 shortest = Math.min(shortest, i - startIdxs.poll());
             
-            // Keeping the startIdxs deque in an order of increasing sequence
-            while(!startIdxs.isEmpty() && prefixSum[i] <= prefixSum[startIdxs.peekLast()])
-                startIdxs.pollLast();
+//             // Keeping the startIdxs deque in an order of increasing sequence
+//             while(!startIdxs.isEmpty() && prefixSum[i] <= prefixSum[startIdxs.peekLast()])
+//                 startIdxs.pollLast();
             
-            // Add the current index to the startIdxs queue
-            startIdxs.add(i);
-        }
+//             // Add the current index to the startIdxs queue
+//             startIdxs.add(i);
+//         }
         
-        return shortest <= nLen ? shortest : -1;
-    }
-}
+//         return shortest <= nLen ? shortest : -1;
+//     }
+// }
