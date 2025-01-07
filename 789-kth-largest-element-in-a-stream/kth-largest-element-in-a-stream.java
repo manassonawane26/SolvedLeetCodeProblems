5676ff682b40b2e2f522b73c9b71fcf6dd5b1edc
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2025-01-07 22:16:59 +0530

    Time: 20 ms (84.15%) | Memory: 50.2 MB (48.89%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.java
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.java
@@ -6,33 +6,51 @@ class KthLargest {
         this.k = k;
         minHeap = new PriorityQueue<>(k);
         for (int num : nums) {
-            minHeap.offer(num);
-            if (minHeap.size() > k) {
-                minHeap.poll();
-            } 
-            // else if (num > minHeap.peek()) {
-            //     minHeap.offer(num);
-            //     if (minHeap.size() > k) {
-            //         minHeap.poll();
-            //     }
-            // }
+            if (minHeap.size() < k) {
+                minHeap.offer(num);
+            } else if (num > minHeap.peek()) {
+                minHeap.offer(num);
+                if (minHeap.size() > k) {
+                    minHeap.poll();
+                }
+            }
         }
     }
-    
+
     public int add(int val) {
-         minHeap.offer(val);
-        if (minHeap.size() > k) {
+        if (minHeap.size() < k) {
+            minHeap.offer(val);
+        } else if (val > minHeap.peek()) {
+            minHeap.offer(val);
             minHeap.poll();
-        } 
-        // if (minHeap.size() < k) {
-        //     minHeap.offer(val);
-        // } else if (val > minHeap.peek()) {
-        //     minHeap.offer(val);
-        //     minHeap.poll();
-        // }
+        }
         return minHeap.peek();
     }
 }
+
+// class KthLargest {
+//     private int k;
+//     private PriorityQueue<Integer> minHeap;
+
+//     public KthLargest(int k, int[] nums) {
+//         this.k = k;
+//         minHeap = new PriorityQueue<>(k);
+//         for (int num : nums) {
+//             minHeap.offer(num);
+//             if (minHeap.size() > k) {
+//                 minHeap.poll();
+//             } 
+//         }
+//     }
+    
+//     public int add(int val) {
+//          minHeap.offer(val);
+//         if (minHeap.size() > k) {
+//             minHeap.poll();
+//         }
+//         return minHeap.peek();
+//     }
+// }
 
 /**
  * Your KthLargest object will be instantiated and called as such:
