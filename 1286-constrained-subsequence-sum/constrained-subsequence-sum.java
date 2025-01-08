f5b3ba13226e2203c9c98edad71eb80ddb364427
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2025-01-08 05:34:18 +0530

    Time: 95 ms (13.78%) | Memory: 56.8 MB (66.54%) - LeetSync

---
+++ 1286-constrained-subsequence-sum/constrained-subsequence-sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int constrainedSubsetSum(int[] nums, int k) {
+        int n=nums.length;
+        int[] dp=new int[n];
+        Arrays.fill(dp,Integer.MIN_VALUE);
+        Queue<Integer> queue=new PriorityQueue<>((a,b)->dp[b]-dp[a]); //Declaring Max heap
+        int res=nums[0];
+        dp[0]=nums[0];
+        queue.offer(0);
+        
+        for(int j=1;j<n;j++){
+            // int i=Math.max(j-k,0);    // get the furthest index possible 
+            while(!queue.isEmpty() && queue.peek() < j-k) queue.poll();     // find the global max in the specified range for that particular j index
+            // int idx=queue.peek();
+            dp[j]=Math.max(dp[queue.peek()]+nums[j],nums[j]);
+            res=Math.max(res,dp[j]);
+            queue.offer(j);
+        }
+        return res;
+    }
+}
