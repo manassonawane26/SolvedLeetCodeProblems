1574bd234d3603443b2f0c2d499ccd5ea900700a
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2025-01-08 05:51:02 +0530

    Time: 96 ms (12.20%) | Memory: 56.1 MB (94.88%) - LeetSync

--- 1286-constrained-subsequence-sum/constrained-subsequence-sum.java
+++ 1286-constrained-subsequence-sum/constrained-subsequence-sum.java
@@ -1,22 +1,39 @@
 class Solution {
     public int constrainedSubsetSum(int[] nums, int k) {
-        int res = Integer.MIN_VALUE;
-        Queue<int[]> maxHeap = new PriorityQueue<>((a,b) -> b[1] - a[1]);
-        for (int i = 0; i < nums.length; i++) {
-            while (!maxHeap.isEmpty() && (i - maxHeap.peek()[0] > k)) {
-                maxHeap.poll();
-            }
-            int temp = 0;
-            if (!maxHeap.isEmpty()) 
-                temp = maxHeap.peek()[1];
-            temp += nums[i];
-            temp = Math.max(temp, nums[i]);
-            res = Math.max(res, temp);
-            maxHeap.add(new int[]{i, temp});
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->b[0]-a[0]);
+        pq.add(new int[]{0, -1});
+        int res=Integer.MIN_VALUE;
+        for(int i=0; i<nums.length; ++i) {
+            while(!pq.isEmpty() && pq.peek()[1]<i-k)
+                pq.poll();
+            // int v[]=pq.peek();
+            int temp=pq.peek()[0]+nums[i];
+            pq.add(new int[]{Math.max(0, temp), i});
+            res=Math.max(res, temp);
         }
         return res;
     }
 }
+
+// class Solution {
+//     public int constrainedSubsetSum(int[] nums, int k) {
+//         int res = Integer.MIN_VALUE;
+//         Queue<int[]> maxHeap = new PriorityQueue<>((a,b) -> b[1] - a[1]);
+//         for (int i = 0; i < nums.length; i++) {
+//             while (!maxHeap.isEmpty() && (i - maxHeap.peek()[0] > k)) {
+//                 maxHeap.poll();
+//             }
+//             int temp = 0;
+//             if (!maxHeap.isEmpty()) 
+//                 temp = maxHeap.peek()[1];
+//             temp += nums[i];
+//             temp = Math.max(temp, nums[i]);
+//             res = Math.max(res, temp);
+//             maxHeap.add(new int[]{i, temp});
+//         }
+//         return res;
+//     }
+// }
 
 
 /*
