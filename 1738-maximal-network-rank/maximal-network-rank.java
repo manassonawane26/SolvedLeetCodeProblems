f7bf4757424c9d5fd635c2e178c11b7db3a4e423
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2025-01-13 22:56:27 +0530

    Time: 4 ms (97.93%) | Memory: 46 MB (24.22%) - LeetSync

--- 1738-maximal-network-rank/maximal-network-rank.java
+++ 1738-maximal-network-rank/maximal-network-rank.java
@@ -1,26 +1,76 @@
-class Solution {
-    public int maximalNetworkRank(int n, int[][] roads) {
-        int[] degree = new int[n];
-        Set<String> roadSet = new HashSet<>();
+// class Solution {
+//     public int maximalNetworkRank(int n, int[][] roads) {
+//         int[] degree = new int[n];
+//         Set<String> roadSet = new HashSet<>();
         
+//         for (int[] road : roads) {
+//             degree[road[0]]++;
+//             degree[road[1]]++;
+//             roadSet.add(road[0] + "," + road[1]);
+//             roadSet.add(road[1] + "," + road[0]);
+//         }
+
+//         int maxRank = 0;
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i+1; j < n; j++) {
+//                 int rank = degree[i] + degree[j];
+//                 if (roadSet.contains(i + "," + j)) {
+//                     rank--;
+//                 }
+//                 maxRank = Math.max(maxRank, rank);
+//             }
+//         }
+
+//         return maxRank;
+//     }
+// }
+
+public class Solution {
+    public int maximalNetworkRank(int n, int[][] roads) {
+        int[] degrees = new int[n];
         for (int[] road : roads) {
-            degree[road[0]]++;
-            degree[road[1]]++;
-            roadSet.add(road[0] + "," + road[1]);
-            roadSet.add(road[1] + "," + road[0]);
+            degrees[road[0]]++;
+            degrees[road[1]]++;
+        }
+        
+        Set<Integer> uniqueDegrees = new HashSet<>();
+        for (int degree : degrees) {
+            uniqueDegrees.add(degree);
+        }
+        
+        List<Integer> sortedDegrees = new ArrayList<>(uniqueDegrees);
+        Collections.sort(sortedDegrees, Collections.reverseOrder());
+
+        int maxDeg = sortedDegrees.get(0);
+        int secondMaxDeg = sortedDegrees.size() > 1 ? sortedDegrees.get(1) : 0;
+
+        int maxCount = 0;
+        for (int degree : degrees) {
+            if (degree == maxDeg) maxCount++;
         }
 
-        int maxRank = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = i+1; j < n; j++) {
-                int rank = degree[i] + degree[j];
-                if (roadSet.contains(i + "," + j)) {
-                    rank--;
-                }
-                maxRank = Math.max(maxRank, rank);
+        int secondMaxCount = 0;
+        for (int degree : degrees) {
+            if (degree == secondMaxDeg) secondMaxCount++;
+        }
+
+        if (maxCount > 1) {
+            int directlyConnected = 0;
+            for (int[] road : roads) {
+                if (degrees[road[0]] == maxDeg && degrees[road[1]] == maxDeg)
+                    directlyConnected++;
             }
+            int possibleConnections = maxCount * (maxCount - 1) / 2;
+            return possibleConnections == directlyConnected ? 2 * maxDeg - 1 : 2 * maxDeg;
         }
 
-        return maxRank;
+        int directConnectionsToSecond = 0;
+        for (int[] road : roads) {
+            if ((degrees[road[0]] == maxDeg && degrees[road[1]] == secondMaxDeg) ||
+                (degrees[road[0]] == secondMaxDeg && degrees[road[1]] == maxDeg))
+                directConnectionsToSecond++;
+        }
+
+        return secondMaxCount == directConnectionsToSecond ? maxDeg + secondMaxDeg - 1 : maxDeg + secondMaxDeg;
     }
 }
