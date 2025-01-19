fadb635373f4ab356ee9dfb3587a43d4926263d3
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2025-01-19 21:11:05 +0530

    Time: 243 ms (25.14%) | Memory: 189.4 MB (18.72%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,27 +1,37 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
+        List<List<Integer>> graph = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            graph.add(new ArrayList<Integer>());
+        }
         for (int[] edge : edges) {
             int u = edge[0];
             int v = edge[1];
-            graph.computeIfAbsent(u, k -> new ArrayList<>()).add(v);
-            graph.computeIfAbsent(v, k -> new ArrayList<>()).add(u);
+            graph.get(u).add(v);
+            graph.get(v).add(u);
         }
-        
         Set<Integer> visited = new HashSet<>();
+
+        //logic to print graph
+        // for(int i=0;i<graph.size();i++){
+        //     System.out.print("Node " +i);
+        //     for(int j=0;j<graph.get(i).size(); j++){
+        //         System.out.print(" => " + graph.get(i).get(j));
+        //     }
+        //     System.out.println();
+        // }
+
         return dfs(source, destination, graph, visited);
     }
     
-    private boolean dfs(int node, int destination, Map<Integer, List<Integer>> graph, Set<Integer> visited) {
-        if (node == destination) {
-            return true;
-        }
+    private boolean dfs(int node, int destination, List<List<Integer>> graph, Set<Integer> visited){
+        if(node == destination) return true;
         visited.add(node);
-        for (int neighbor : graph.getOrDefault(node, new ArrayList<>())) {
-            if (!visited.contains(neighbor)) {
-                if (dfs(neighbor, destination, graph, visited)) {
-                    return true;
-                }
+        
+        for(int i : graph.get(node)) {
+            // System.out.println("Node: " +node);
+            if(!visited.contains(i)){
+                if(dfs(i, destination, graph, visited)) return true;
             }
         }
         return false;
