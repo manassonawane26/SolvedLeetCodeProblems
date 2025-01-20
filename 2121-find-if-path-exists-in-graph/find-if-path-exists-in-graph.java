f0382f77fc17ad83d6397f9e724ffb2b5beb0458
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2025-01-20 22:27:35 +0530

    Time: 87 ms (75.85%) | Memory: 125.7 MB (94.17%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,3 +1,43 @@
+// class Solution {
+//     public boolean validPath(int n, int[][] edges, int source, int destination) {
+//         List<List<Integer>> graph = new ArrayList<>();
+//         for(int i=0; i<n; i++){
+//             graph.add(new ArrayList<Integer>());
+//         }
+//         for (int[] edge : edges) {
+//             int u = edge[0];
+//             int v = edge[1];
+//             graph.get(u).add(v);
+//             graph.get(v).add(u);
+//         }
+//         Set<Integer> visited = new HashSet<>();
+
+//         //logic to print graph
+//         // for(int i=0;i<graph.size();i++){
+//         //     System.out.print("Node " +i);
+//         //     for(int j=0;j<graph.get(i).size(); j++){
+//         //         System.out.print(" => " + graph.get(i).get(j));
+//         //     }
+//         //     System.out.println();
+//         // }
+
+//         return dfs(source, destination, graph, visited);
+//     }
+    
+//     private boolean dfs(int node, int destination, List<List<Integer>> graph, Set<Integer> visited){
+//         if(node == destination) return true;
+//         visited.add(node);
+        
+//         for(int i : graph.get(node)) {
+//             // System.out.println("Node: " +node);
+//             if(!visited.contains(i) && dfs(i, destination, graph, visited)) 
+//                 return true;
+
+//         }
+//         return false;
+//     }
+// }
+
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         List<List<Integer>> graph = new ArrayList<>();
@@ -21,18 +61,27 @@ class Solution {
         //     System.out.println();
         // }
 
-        return dfs(source, destination, graph, visited);
+        return bfs(source, destination, graph, visited);
     }
     
-    private boolean dfs(int node, int destination, List<List<Integer>> graph, Set<Integer> visited){
+    private boolean bfs(int node, int destination, List<List<Integer>> graph, Set<Integer> visited){
         if(node == destination) return true;
+        Queue<Integer> q= new LinkedList<>();
         visited.add(node);
-        
-        for(int i : graph.get(node)) {
-            // System.out.println("Node: " +node);
-            if(!visited.contains(i) && dfs(i, destination, graph, visited)) 
-                return true;
-
+        q.add(node);
+        while(!q.isEmpty()){
+            int curr = q.poll();
+            List<Integer> connNodes = graph.get(curr);
+            if(connNodes!=null){
+                for(int i: connNodes){
+                    if(!visited.contains(i)){
+                        if(i == destination) return true;
+                        visited.add(i);
+                        q.add(i);
+                        
+                    }
+                }
+            }
         }
         return false;
     }
