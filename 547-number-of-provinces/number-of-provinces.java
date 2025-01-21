c280283393abd891ab7be702690a967e7d7c7668
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2025-01-21 06:37:02 +0530

    Time: 1 ms (88.87%) | Memory: 48.3 MB (13.16%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,4 +1,34 @@
 //DFS
+// class Solution {
+//     public int findCircleNum(int[][] grid) {
+//         int n = grid.length;
+//         boolean[] vis = new boolean[n];
+
+//         int count=0;
+//         for (int i = 0; i < n; i++) {
+//             if (!vis[i]) {
+//                 dfs(grid, vis, i);
+//                 count++;
+//             }
+//         }
+
+//         return count;
+//     }
+    
+//     public void dfs(int[][] grid, boolean[] vis, int u) {
+//         vis[u] = true;
+//         for (int v = 0; v < grid.length; v++) {
+//             if (grid[u][v]==1) {
+//                 if (!vis[v]) {
+//                     dfs(grid, vis, v);
+//                 }
+//             }
+//         }
+//     }
+
+// }
+
+//BFS
 class Solution {
     public int findCircleNum(int[][] grid) {
         int n = grid.length;
@@ -7,7 +37,7 @@ class Solution {
         int count=0;
         for (int i = 0; i < n; i++) {
             if (!vis[i]) {
-                dfs(grid, vis, i);
+                bfs(grid, vis, i);
                 count++;
             }
         }
@@ -15,13 +45,21 @@ class Solution {
         return count;
     }
     
-    public void dfs(int[][] grid, boolean[] vis, int u) {
-        vis[u] = true;
-        for (int v = 0; v < grid.length; v++) {
-            if (grid[u][v]==1) {
-                if (!vis[v]) {
-                    dfs(grid, vis, v);
-                }
+    public void bfs(int[][] grid, boolean[] vis, int U) {
+        Queue<Integer> q = new LinkedList<>();
+
+        vis[U] = true;
+        q.add(U);
+
+        while (!q.isEmpty()) {
+            int u = q.poll();
+            for (int v = 0; v < grid.length; v++) {
+                if (grid[u][v]==1) {
+                    if (!vis[v]) {
+                        vis[v] = true;
+                        q.add(v);
+                    }
+                }   
             }
         }
     }
