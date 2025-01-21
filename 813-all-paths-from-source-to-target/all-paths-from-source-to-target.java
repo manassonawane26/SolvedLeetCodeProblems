3bbc2c7a6a6316d37778df00892a07890b8a6391
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2025-01-21 05:38:48 +0530

    Time: 1 ms (100.00%) | Memory: 46.4 MB (46.48%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.java
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.java
@@ -1,20 +1,41 @@
-class Solution {
-    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        List<List<Integer>> ans = new ArrayList<>();
-        findPath(0, graph, ans, new ArrayList<>());
-        return ans;
-    }
-    private void findPath(int node, int[][] graph, List<List<Integer>> ans, List<Integer> curr){
-        curr.add(node);
-        if(node == graph.length -1){
-            ans.add(new ArrayList<>(curr));
-            curr.remove(curr.size()-1);
-            return;
-        }
+// class Solution {
+//     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+//         List<List<Integer>> ans = new ArrayList<>();
+//         findPath(0, graph, ans, new ArrayList<>());
+//         return ans;
+//     }
+//     private void findPath(int node, int[][] graph, List<List<Integer>> ans, List<Integer> curr){
+//         curr.add(node);
+//         if(node == graph.length -1){
+//             ans.add(new ArrayList<>(curr));
+//             curr.remove(curr.size()-1);
+//             return;
+//         }
 
-        for(int i: graph[node]){
-            findPath(i, graph, ans, curr);
-        }
-        curr.remove(curr.size()-1);
+//         for(int i: graph[node]){
+//             findPath(i, graph, ans, curr);
+//         }
+//         curr.remove(curr.size()-1);
+//     }
+// }
+
+class Solution {
+
+    public void backtrack(List<List<Integer>> result, List<Integer> path, int[][] graph, int n) {
+        path.add(n);
+
+        if(n == graph.length - 1)
+            result.add(new ArrayList<>(path));
+        else 
+            for(int v : graph[n]) 
+                backtrack(result, path, graph, v);
+
+        path.remove(path.size() -1);
+    }
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<List<Integer>>  result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), graph, 0);
+
+        return result;
     }
 }
