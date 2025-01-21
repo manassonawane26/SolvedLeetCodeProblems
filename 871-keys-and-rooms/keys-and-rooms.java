53ab24d834ed81cf956e6b83f653e4b4e7a2c773
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2025-01-21 05:58:27 +0530

    Time: 2 ms (57.26%) | Memory: 44.6 MB (34.53%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.java
+++ 871-keys-and-rooms/keys-and-rooms.java
@@ -24,26 +24,50 @@
 //     }
 // }
 
-//BFS
+//BFS 1
+// class Solution {
+//     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+//         int n = rooms.size();
+//         Set vis = new HashSet<>();
+//         Queue<Integer> q = new LinkedList<>();
+//         q.add(0);
+//         vis.add(0);
+//         int count = 1;
+//         while(!q.isEmpty()){
+//             int curr = q.poll();
+//             for(int i: rooms.get(curr)){
+//                 if(!vis.contains(i)){
+//                     q.offer(i);
+//                     vis.add(i);
+//                     count++;
+//                     System.out.println(i);
+//                 }
+//             }
+//         }
+//         return vis.size() == n;
+//     }
+// }
+
+//BFS2
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
         int n = rooms.size();
-        Set vis = new HashSet<>();
-        Queue<Integer> q = new LinkedList<>();
-        q.add(0);
-        vis.add(0);
+        boolean[] vis = new boolean[n];
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        vis[0] = true;
         int count = 1;
-        while(!q.isEmpty()){
-            int curr = q.poll();
-            for(int i: rooms.get(curr)){
-                if(!vis.contains(i)){
-                    q.offer(i);
-                    vis.add(i);
+
+        while (!stack.isEmpty()) {
+            int currentRoom = stack.pop();
+            for (int key : rooms.get(currentRoom)) {
+                if (!vis[key]) {
+                    stack.push(key);
+                    vis[key] = true;
                     count++;
-                    System.out.println(i);
                 }
             }
         }
-        return vis.size() == n;
+        return count == n;
     }
 }
