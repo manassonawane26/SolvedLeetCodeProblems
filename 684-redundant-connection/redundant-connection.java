1c2802ca483ad7d0e3300ba6deb8d5ed1a6a44c8
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2025-01-25 04:56:33 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (62.70%) - LeetSync

--- 684-redundant-connection/redundant-connection.java
+++ 684-redundant-connection/redundant-connection.java
@@ -1,5 +1,45 @@
+// class Solution {
+//     int root[], size[];
+
+//     public int findParent(int node) {
+//         if (node == root[node])
+//             return node;
+//         return root[node] = findParent(root[node]);
+//     }
+
+//     public boolean union(int u, int v) {
+//         int rootU = findParent(u);
+//         int rootV = findParent(v);
+//         if (rootV == rootU)
+//             return false;
+//         else if (size[rootU] > size[rootV]) {
+//             size[rootU] += size[rootV];
+//             root[rootV] = rootU;
+//         } else {
+//             size[rootV] += size[rootU];
+//             root[rootU] = rootV;
+//         }
+//         return true;
+//     }
+
+//     public int[] findRedundantConnection(int[][] edges) {
+//         int n = edges.length, k = 0;
+//         root = new int[n + 1];
+//         size = new int[n + 1];
+//         Arrays.fill(size, 1);
+//         for (int i = 0; i <= n; i++)
+//             root[i] = i;
+//         for (int[] edge : edges) {
+//             if (!union(edge[0], edge[1]))
+//                 break;
+//             k++;
+//         }
+//         return new int[] { edges[k][0], edges[k][1] };
+//     }
+// }
+
 class Solution {
-    int root[], size[];
+    int root[], rank[];
 
     public int findParent(int node) {
         if (node == root[node])
@@ -10,13 +50,14 @@ class Solution {
     public boolean union(int u, int v) {
         int rootU = findParent(u);
         int rootV = findParent(v);
-        if (rootV == rootU)
+        if (rootV == rootU){
+            // root[rootV] = rootU;
+            // rank[rootU] = rank[rootU]+1;
             return false;
-        else if (size[rootU] > size[rootV]) {
-            size[rootU] += size[rootV];
+        }
+        else if (rank[rootU] > rank[rootV]) {
             root[rootV] = rootU;
         } else {
-            size[rootV] += size[rootU];
             root[rootU] = rootV;
         }
         return true;
@@ -25,8 +66,8 @@ class Solution {
     public int[] findRedundantConnection(int[][] edges) {
         int n = edges.length, k = 0;
         root = new int[n + 1];
-        size = new int[n + 1];
-        Arrays.fill(size, 1);
+        rank = new int[n + 1];
+        Arrays.fill(rank, 1);
         for (int i = 0; i <= n; i++)
             root[i] = i;
         for (int[] edge : edges) {
