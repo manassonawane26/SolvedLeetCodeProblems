f65da2c6cc5f39eed332da236c09efae27dcd827
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2025-01-25 23:13:32 +0530

    Time: 1170 ms (11.38%) | Memory: 75.3 MB (5.69%) - LeetSync

--- 1887-minimum-degree-of-a-connected-trio-in-a-graph/minimum-degree-of-a-connected-trio-in-a-graph.java
+++ 1887-minimum-degree-of-a-connected-trio-in-a-graph/minimum-degree-of-a-connected-trio-in-a-graph.java
@@ -1,32 +1,48 @@
 class Solution {
-public int minTrioDegree(int n, int[][] edges) {
-	// to store edge information
-    boolean[][] graph = new boolean[n+1][n+1];
-	//to store inDegrees to a node(NOTE: here inDegree and outDegree are same because it is Undirected graph)
-    int[] inDegree = new int[n+1];
-    
-    for(int[] edge : edges) {
-        graph[edge[0]][edge[1]] = true;
-        graph[edge[1]][edge[0]] = true;
+    public int minTrioDegree(int n, int[][] edges) {
+        int minimumDegree = Integer.MAX_VALUE;
         
-        inDegree[edge[0]]++;
-        inDegree[edge[1]]++;
-    }
-    
-    int result = Integer.MAX_VALUE;
-    for(int i=1; i<=n; i++) {
-        for(int j=i+1; j<=n; j++) {
-            if(graph[i][j]) {
-                for(int k=j+1; k<=n; k++) {
-                    if(graph[i][k] && graph[j][k]) {
-                        result = Math.min(result, inDegree[i] + inDegree[j] + inDegree[k] - 6);
-                    }
+        // arrange the array edges such that if e[0] > e[1], swap e[1] and e[0]
+        for (int [] e : edges) {
+            if (e[0] > e[1]) {
+                int temp = e[0];
+                e[0] = e[1];
+                e[1] = temp;
+            }
+        }
+        // now sort the edges. 2 conditions to check
+        // if e1[0] == e2[0] then sort based on e1[1] and e2[1]
+        // else do the sorting based on e1[0] and e2[0]
+        Arrays.sort(edges, (e1, e2) -> e1[0] == e2[0] ? e1[1] - e2[1] : e1[0] - e2[0]);
+        
+        // build the graph and we dont want to consider duplicates, so use a set to track the neighbours
+        Map<Integer, Set<Integer>> graph = new HashMap<>();
+        for (int [] e : edges) {
+            graph.computeIfAbsent(e[0], x -> new HashSet<>()).add(e[1]);
+            graph.computeIfAbsent(e[1], x -> new HashSet<>()).add(e[0]);
+        }
+        
+        // again go through the edges
+        // e[0] = first e[1] = second now from i = second + 1 to n -> where third = i, then check
+        // if graph have third
+        // if the third have neighbours first in it.
+        // if the third have neighbours second in it.
+        // if it holds true, then compute each of the size eg : graph.get(first).size() + secondSize + thirdSize - 6
+        // then greedily consider the value for degree
+        
+        for (int [] e : edges) {
+            int first = e[0];
+            int second = e[1];
+            for (int i = second + 1; i<=n; i++) {
+                int third = i;
+                if (graph.containsKey(third) && graph.get(third).contains(first) && graph.get(third).contains(second)) {
+                    int currentDegree = graph.get(first).size() + graph.get(second).size() + graph.get(third).size() - 6;
+                    minimumDegree = Math.min(minimumDegree, currentDegree);
                 }
             }
         }
+        
+        // for the last check if our minimumDegree is still Integer.MAX_Value return -1 else return minimumDegree
+        return minimumDegree == Integer.MAX_VALUE ? -1 : minimumDegree;
     }
-    
-    
-    return result == Integer.MAX_VALUE ? -1 : result;
-}
 }
