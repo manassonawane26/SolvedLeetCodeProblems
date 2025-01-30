f18993d21d3ec31b4adc59d1169364c43b1c44e7
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2025-01-30 22:05:53 +0530

    Time: 6 ms (53.26%) | Memory: 46.8 MB (6.73%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -7,7 +7,7 @@ class Solution {
         }
         int n = prerequisites.length;
         for (int i=0; i<n; i ++ ){
-            adjList.get(prerequisites[i][0]).add(prerequisites[i][1]);
+            adjList.get(prerequisites[i][1]).add(prerequisites[i][0]);
         }
         
         int indegree[] = new int[numCourses];
@@ -23,19 +23,19 @@ class Solution {
         }
 
         int[] topo = new int[numCourses];
-        int idx = numCourses-1;
+        int idx = 0; // numCourses-1;
 
         while(!q.isEmpty()){
             int node = q.peek();
             q.remove();
-            topo[idx--] = node;
+            topo[idx++] = node;
             for(int it : adjList.get(node)){
                 indegree[it]--;
                 if(indegree[it] == 0) q.add(it);
             }
         }
 
-        if(idx == -1) return topo;
+        if(idx == numCourses) return topo;
         return new int[] {};
     }
 }
