c2b284f57b888c25d295a1ab72bded3456dfafa3
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2025-01-30 22:05:09 +0530

    Time: 7 ms (36.58%) | Memory: 45.9 MB (70.46%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        // Convert the adjacency matrix to an adjacency list for convenience
+        List<List<Integer>> adjList = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            adjList.add(new ArrayList<>());
+        }
+        int n = prerequisites.length;
+        for (int i=0; i<n; i ++ ){
+            adjList.get(prerequisites[i][0]).add(prerequisites[i][1]);
+        }
+        
+        int indegree[] = new int[numCourses];
+        for(int i=0; i<numCourses; i++){
+            for(int it: adjList.get(i)){
+                indegree[it]++;
+            }
+        }
+        Queue<Integer> q = new LinkedList<Integer>();
+        for(int i=0;i<numCourses; i++){
+            if(indegree[i]==0)
+                q.add(i);
+        }
+
+        int[] topo = new int[numCourses];
+        int idx = numCourses-1;
+
+        while(!q.isEmpty()){
+            int node = q.peek();
+            q.remove();
+            topo[idx--] = node;
+            for(int it : adjList.get(node)){
+                indegree[it]--;
+                if(indegree[it] == 0) q.add(it);
+            }
+        }
+
+        if(idx == -1) return topo;
+        return new int[] {};
+    }
+}
