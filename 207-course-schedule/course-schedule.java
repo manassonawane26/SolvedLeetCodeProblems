3dd3322c975b6220002492a8f1250fdf9e29f05d
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2025-01-30 22:06:51 +0530

    Time: 7 ms (50.51%) | Memory: 46.1 MB (18.05%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        // Convert the adjacency matrix to an adjacency list for convenience
+        List<List<Integer>> adjList = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            adjList.add(new ArrayList<>());
+        }
+        int n = prerequisites.length;
+        for (int i=0; i<n; i ++ ){
+            adjList.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+        
+        int indegree[] = new int[numCourses];
+        for(int i=0; i<numCourses; i++){
+            for(int it: adjList.get(i)){
+                indegree[it]++;
+            }
+        }
+        Queue<Integer> q = new LinkedList<Integer>();
+        for(int i=0;i<numCourses; i++){
+            if(indegree[i]==0)
+                q.add(i);
+        }
+
+        int[] topo = new int[numCourses];
+        int idx = 0; // numCourses-1;
+
+        while(!q.isEmpty()){
+            int node = q.peek();
+            q.remove();
+            topo[idx++] = node;
+            for(int it : adjList.get(node)){
+                indegree[it]--;
+                if(indegree[it] == 0) q.add(it);
+            }
+        }
+
+        if(idx == numCourses) return true;
+        return false;
+    }
+}
