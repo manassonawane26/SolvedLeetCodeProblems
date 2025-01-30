539f1f3c7605219ec45aa59b4e61b1caf8482c35
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2025-01-30 21:33:47 +0530

    Time: 6 ms (14.89%) | Memory: 45 MB (65.73%) - LeetSync

---
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -0,0 +1,78 @@
+// class Solution {
+//     public boolean isBipartite(int[][] graph) {
+//         int v = graph.length;
+//         int[] color =  new int[v];
+//         Arrays.fill(color, -1);
+//         for(int i=0;i<v; i++) {
+//             if(color[i]==-1){
+//                 if(!bfsCheckBipartite(i, graph, color))
+//                     return false;
+//             }
+//         }
+//         return true;
+//     }
+
+//     public boolean bfsCheckBipartite(int start, int[][] graph, int[] color){
+//         Queue<Integer> q = new LinkedList<>();
+//         q.add(start);
+//         color[start] = 0;
+//         while(!q.isEmpty()){
+//             int currNode = q.poll();
+//             int currColor = color[currNode];
+//             for(int i =0; i< graph[i].length; i++){
+//                 if(graph[start][i] == 1){
+//                     // System.out.println(start + " " + i);
+//                     if(color[i] == -1){
+//                         color[i] = 1 - currColor;
+//                         q.add(i);
+//                     } else if( color[i] == currColor) return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+import java.util.*;
+
+class Solution {
+    private boolean dfs(int node, int colour, List<List<Integer>> graph, int[] color) {
+        color[node] = colour; // Color the current node
+        for (int neighbor : graph.get(node)) {
+            if (color[neighbor] == -1) { 
+                // If neighbor is uncolored, recursively color it with the opposite color
+                if (!dfs(neighbor, 1 - colour, graph, color)) {
+                    return false; // Conflict detected
+                }
+            } else if (color[neighbor] == colour) {
+                return false; // Conflict detected
+            }
+        }
+        return true; // Successfully colored this component
+    }
+
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        int[] color = new int[n]; // Color array: -1 -> uncolored, 0 -> color1, 1 -> color2
+        Arrays.fill(color, -1);
+
+        // Convert the adjacency matrix to an adjacency list for convenience
+        List<List<Integer>> adjList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adjList.add(new ArrayList<>());
+            for (int j : graph[i]) {
+                adjList.get(i).add(j);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (color[i] == -1) { 
+                // If the node is uncolored, start DFS from it
+                if (!dfs(i, 0, adjList, color)) {
+                    return false; // If any component is not bipartite, return false
+                }
+            }
+        }
+        return true; // All components are bipartite
+    }
+}
