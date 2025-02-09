0b6fbef057fcde7cf8a62992ec3e207a7be9eece
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2025-02-09 13:00:56 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (91.94%) - LeetSync

--- 1329-minimum-cost-to-move-chips-to-the-same-position/minimum-cost-to-move-chips-to-the-same-position.java
+++ 1329-minimum-cost-to-move-chips-to-the-same-position/minimum-cost-to-move-chips-to-the-same-position.java
@@ -3,13 +3,9 @@ class Solution {
         int evenCoins = 0;
         int oddCoins = 0;
 
-        for (int i = 0; i < position.length; i++) { 
-            if (position[i] % 2 == 0) {  
-                evenCoins++;
-            } else {
-                oddCoins++;
-            }
-        }
+        for (int i = 0; i < position.length; i++)
+            if (position[i] % 2 == 0) evenCoins++;
+            else oddCoins++;
 
         return Math.min(evenCoins, oddCoins);
     }
