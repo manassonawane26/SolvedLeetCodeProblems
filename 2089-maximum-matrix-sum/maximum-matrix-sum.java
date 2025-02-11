254c6d54dff110b6f9f61bee9791945c99e28b71
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2025-02-11 09:19:02 +0530

    Time: 6 ms (85.81%) | Memory: 55.1 MB (56.77%) - LeetSync

--- 2089-maximum-matrix-sum/maximum-matrix-sum.java
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.java
@@ -4,16 +4,15 @@ class Solution {
     long sum = 0;
     int negCount = 0; 
     for(int i=0; i<matrix.length; i++)
-    for(int j=0; j<matrix[0].length; j++)
-    {
-     if(matrix[i][j]<0)
-     negCount++;
-     int ab = Math.abs(matrix[i][j]);
-     min = Math.min(min, ab);
-     sum += ab;    
-    }
+        for(int j=0; j<matrix[0].length; j++){
+            if(matrix[i][j]<0)
+                negCount++;
+            int ab = Math.abs(matrix[i][j]);
+            min = Math.min(min, ab);
+            sum += ab;    
+        }
     if(negCount%2==0)
-    return sum; 
+        return sum; 
     return sum - 2*min;
 }
 }
