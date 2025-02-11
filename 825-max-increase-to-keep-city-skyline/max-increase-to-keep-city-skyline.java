69a14162eed5ca078de9577a48889be52df48c9b
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2025-02-11 06:21:26 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (42.12%) - LeetSync

--- 825-max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.java
+++ 825-max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.java
@@ -4,17 +4,15 @@ class Solution {
         int rMax[] = new int[n];
         int cMax[] = new int[n];
         for(int i=0; i<n; i++){
-            int row=0, col=0;
             for(int j=0; j<n; j++){
-                row=Math.max(row, grid[i][j]);
-                col=Math.max(col, grid[j][i]);
-                rMax[i]=row;
-                cMax[i]=col;
+                rMax[i]=Math.max(rMax[i], grid[i][j]);
+                cMax[i]=Math.max(cMax[i], grid[j][i]);
             }
         }
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                sum+=(Math.min(rMax[i], cMax[j]) - grid[i][j]);
+                int temp = Math.min(rMax[i], cMax[j]);
+                sum+=(temp > grid[i][j] ? temp - grid[i][j]: 0);
             }
         }
         return sum;
