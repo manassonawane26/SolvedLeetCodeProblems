49ee054c9eaeb51ae65b6e366d4994c18471253a
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2025-02-13 05:24:34 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (37.69%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -6,12 +6,13 @@ class Solution {
         fibo[0] = 0;
         fibo[1] = 1;
         fibo[2] = 1;
+        fibo[3] = 2;
         return recursion(n, fibo);
 
     }
     int recursion(int n, int[] fibo){
         if(fibo[n] > 0) return fibo[n];
-        if(n<=2) return n;
+        //if(n<=2) return n;
 
         // if(n<3) return 1;
         fibo[n]=recursion(n-1, fibo) + recursion(n-2, fibo) + recursion(n-3, fibo);
