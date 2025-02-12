fd5d307aca975b22ea6ad6219eb8a7a25b1e084a
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2025-02-12 08:19:50 +0530

    Time: 6 ms (92.87%) | Memory: 41.1 MB (58.86%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
@@ -1,26 +1,48 @@
+// class Solution {
+//     int n; // length of nums
+//     int max; // max of betwise-OR of nums
+//     int result = 0; // result
+//     public int countMaxOrSubsets(int[] nums) {
+//         n = nums.length;
+//         max = nums[0]; 
+//         for(int i = 1; i < nums.length; i++) //going through nums to find betwise-OR
+//             max = max | nums[i];
+//         backtrack(0,new ArrayList<>(),nums); // call magic function which go over all possible subsets of nums 
+//         return result;
+//     }
+//         public void backtrack(int first, ArrayList<Integer> curr, int [] nums){
+//         if(curr.size() > 0 && curr.get(curr.size() - 1) == max) // checking if current betwise-OR  equal the maximum 
+//             result++;
+//         for(int i = first; i < n; i++){
+//             if(curr.size() > 0) 
+//                 curr.add(nums[i] | curr.get(curr.size() - 1)); 
+//             else
+//                 curr.add(nums[i]);
+//             backtrack(i + 1, curr, nums);
+// 	//I would highly suggest debug this code to understand how this "remove" part works 
+//             curr.remove(curr.size() - 1); //important step we removing last element to maintain correct subset on the next iteration
+//         }
+//     }
+// }
+
 class Solution {
-    int n; // length of nums
-    int max; // max of betwise-OR of nums
-    int result = 0; // result
+    int max = 0;
+    int freq = 0;
     public int countMaxOrSubsets(int[] nums) {
-        n = nums.length;
-        max = nums[0]; 
-        for(int i = 1; i < nums.length; i++) //going through nums to find betwise-OR
-            max = max | nums[i];
-        backtrack(0,new ArrayList<>(),nums); // call magic function which go over all possible subsets of nums 
-        return result;
+        getSubsets(nums, 0, 0);
+        return freq;
     }
-        public void backtrack(int first, ArrayList<Integer> curr, int [] nums){
-        if(curr.size() > 0 && curr.get(curr.size() - 1) == max) // checking if current betwise-OR  equal the maximum 
-            result++;
-        for(int i = first; i < n; i++){
-            if(curr.size() > 0) 
-                curr.add(nums[i] | curr.get(curr.size() - 1)); 
-            else
-                curr.add(nums[i]);
-            backtrack(i + 1, curr, nums);
-	//I would highly suggest debug this code to understand how this "remove" part works 
-            curr.remove(curr.size() - 1); //important step we removing last element to maintain correct subset on the next iteration
+
+    void getSubsets(int[] nums, int idx, int curr) {
+        if (curr > max) {
+            max = curr;
+            freq = 1;
+        } else if (curr == max) {
+            freq++;
+        }
+
+        for (int i = idx; i < nums.length; i++) {
+            getSubsets(nums, i+1, curr | nums[i]);
         }
     }
 }
