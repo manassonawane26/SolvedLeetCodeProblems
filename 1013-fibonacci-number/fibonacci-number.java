b711098d5788f4b2112ab9a2e777161cf95e437e
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2025-02-12 08:48:58 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (6.17%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,7 +1,14 @@
 class Solution {
     public int fib(int n) {
+        int[] fibo = new int[n+1];
+        return recursion(n, fibo);
+    }
+    int recursion(int n, int[] fibo){
+        if(fibo[n] > 0) return fibo[n];
         if(n<2) return n;
+
         // if(n<3) return 1;
-        return fib(n-1)+fib(n-2);
+        fibo[n]=recursion(n-1, fibo) + recursion(n-2, fibo);
+        return fibo[n];
     }
 }
