63cfaf7f3ba9274e40f30f4b749c1c43a1335684
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2025-02-18 22:30:13 +0530

    Time: 1 ms (83.93%) | Memory: 42.8 MB (94.35%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,23 +1,50 @@
+// class Solution {
+//     public List<String> generateParenthesis(int n) {
+//         List<String> res = new ArrayList<String>();
+//         recurse(res, 0, 0, "", n);
+//         return res;
+//     }
+    
+//     public void recurse(List<String> res, int left, int right, String s, int n) {
+//         if (s.length() == n * 2) {
+//             res.add(s);
+//             return;
+//         }
+        
+//         if (left < n) {
+//             recurse(res, left + 1, right, s + "(", n);
+//         }
+        
+//         if (right < left) {
+//             recurse(res, left, right + 1, s + ")", n);
+//         }
+//     }
+// 	// See above tree diagram with parameters (left, right, s) for better understanding
+// }
+
+
 class Solution {
     public List<String> generateParenthesis(int n) {
-        List<String> res = new ArrayList<String>();
-        recurse(res, 0, 0, "", n);
+        List<String> res = new ArrayList<>();
+        genParanthesisHelper(res,new StringBuilder(),n,n);
         return res;
     }
-    
-    public void recurse(List<String> res, int left, int right, String s, int n) {
-        if (s.length() == n * 2) {
-            res.add(s);
-            return;
+
+    private void genParanthesisHelper(List<String> res,StringBuilder sb,int open,int close){
+        if(close < open) return;
+        if(close == 0 && open == 0){
+            res.add(sb.toString());
         }
-        
-        if (left < n) {
-            recurse(res, left + 1, right, s + "(", n);
+       
+        if(open > 0){
+            sb.append("(");
+            genParanthesisHelper(res,sb,open-1,close);
+            sb.deleteCharAt(sb.length()-1);
         }
-        
-        if (right < left) {
-            recurse(res, left, right + 1, s + ")", n);
+        if(close > 0){
+            sb.append(")");
+            genParanthesisHelper(res,sb,open,close-1);
+            sb.deleteCharAt(sb.length()-1);
         }
     }
-	// See above tree diagram with parameters (left, right, s) for better understanding
 }
