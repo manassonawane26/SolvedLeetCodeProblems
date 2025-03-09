84380aa0b25b5253b31c3d9797b5dda9fd784a6c
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2025-03-09 20:58:18 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (67.91%) - LeetSync

--- 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.java
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.java
@@ -14,26 +14,47 @@
  * }
  */
 class Solution {
+    private int deepest=0;
+    private TreeNode node;
 	public TreeNode lcaDeepestLeaves(TreeNode root) {
-		if(root == null){
-			return null;
-		}
+        helper(root,0);
+        return node;
+    }
+    private int helper(final TreeNode root, final int depth) {
+        deepest = Math.max(deepest, depth);
+        if(root == null)
+            return depth;
 
-		int left = height(root.left);
-		int right = height(root.right);
-		if(left == right){
-			return root;
-		}else if(left > right){
-			return lcaDeepestLeaves(root.left);
-		}else{
-			return lcaDeepestLeaves(root.right);
-		}
-	}
+        int left = helper(root.left, depth+1);
+        int right = helper(root.right, depth+1);
 
-	private int height(TreeNode root){
-		if(root == null){
-			return 0;
-		}
-		return 1 + Math.max(height(root.left), height(root.right));
-	}
+        if(left == deepest && left == right)
+            node=root;
+        return Math.max(left, right);
+    }
 }
+
+// class Solution {
+// 	public TreeNode lcaDeepestLeaves(TreeNode root) {
+// 		if(root == null){
+// 			return null;
+// 		}
+
+// 		int left = height(root.left);
+// 		int right = height(root.right);
+// 		if(left == right){
+// 			return root;
+// 		}else if(left > right){
+// 			return lcaDeepestLeaves(root.left);
+// 		}else{
+// 			return lcaDeepestLeaves(root.right);
+// 		}
+// 	}
+
+// 	private int height(TreeNode root){
+// 		if(root == null){
+// 			return 0;
+// 		}
+// 		return 1 + Math.max(height(root.left), height(root.right));
+// 	}
+// }
