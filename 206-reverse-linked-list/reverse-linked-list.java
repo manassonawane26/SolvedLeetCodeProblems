87abc3cd61f8c9be96f4c7cb5f15dc56dfb8c6fa
Manas Sonawane <61977582+manassonawane26@users.noreply.github.com>
2025-03-15 01:06:25 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (91.76%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -1,13 +1,32 @@
-class Solution {    
-    public ListNode reverseList(ListNode head) {
-        return revL(null,head);    
-    }
+// class Solution {    
+//     public ListNode reverseList(ListNode head) {
+//         return revL(null,head);    
+//     }
 
-    public ListNode revL(ListNode pre,ListNode cur){
-        if(cur==null) // Tail of LinkedList 
-            return pre; //because pre on last node and cur on null 
-        ListNode nxt=cur.next; //store the reference to next Node
-        cur.next=pre; //reversing Link
-        return revL(cur,nxt); //Trust Recursion and apply it on current and next Node 
+//     public ListNode revL(ListNode pre,ListNode cur){
+//         if(cur==null) // Tail of LinkedList 
+//             return pre; //because pre on last node and cur on null 
+//         ListNode nxt=cur.next; //store the reference to next Node
+//         cur.next=pre; //reversing Link
+//         return revL(cur,nxt); //Trust Recursion and apply it on current and next Node 
+//     }
+// }
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head==null || head.next==null) return head;
+        ListNode prev=head;
+        ListNode curr=head.next;
+        ListNode nxt=head.next;
+        while(curr.next!=null){
+            // curr=curr.next;
+            nxt=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=nxt;
+        }
+        curr.next=prev;
+        head.next=null;
+        return curr;
     }
 }
